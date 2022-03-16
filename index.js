7c9ab5b82d48a93e6d5ac90e37a1ff455033312a
foghill <ben@thefoghill.com>
2022-03-16 15:13:33 -0400

    commit tree aef61070e59e75d441272d8e049a20936ee31b13

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+let configurationObject ={
+    method: 'POST',
+    headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+};
+
+function submitData(name,email){
+configurationObject.body = JSON.stringify({"name": name, "email": email})
+return fetch("http://localhost:3000/users",configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+    document.body.innerHTML=object.id
+  })
+  .catch(function (error) {
+    document.body.innerHTML = error.message
+    // error.message
+  });
+};
